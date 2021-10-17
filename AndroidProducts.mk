5a85544f91dd95747983653f1fb20cf07f8392a4
EcrosoftXiao <xy19990903@gmail.com>
2021-10-17 14:32:23 +0800

    Arrowos bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cepheus.mk
+    $(LOCAL_DIR)/arrow_cepheus.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cepheus-user \
-    aosp_cepheus-userdebug \
-    aosp_cepheus-eng
+    arrow_cepheus-user \
+    arrow_cepheus-userdebug \
+    arrow_cepheus-eng
--- arrow_cepheus.mk
+++ arrow_cepheus.mk
@@ -22,17 +22,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common PE stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := aosp_cepheus
+PRODUCT_NAME := arrow_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9
 PRODUCT_MANUFACTURER := Xiaomi
+
+DEVICE_MAINTAINER := Evasi0nXiao
+ARROW_GAPPS := true
 
 # Include firmware
 $(call inherit-product, vendor/xiaomi-firmware/cepheus/firmware.mk)
